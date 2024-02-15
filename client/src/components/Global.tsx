8272a835f9e51b31c37a2a76833ef32ee11b552b
Petr D. Svoboda <petrdsvoboda@gmail.com>
2024-02-15 19:57:24 +0100

    refactor(font): setup font via stylesheet link

--- client/src/components/Global.tsx
+++ client/src/components/Global.tsx
@@ -3,10 +3,15 @@ import reset from "styled-reset";
 import { normalize } from "styled-normalize";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://rsms.me/inter/inter.css');
-  ${normalize}
-  ${reset}
-  body {
-    font-family: 'Inter', sans-serif;
-  }
+    ${normalize}
+    ${reset}
+    
+    :root {
+       font-family: Inter, sans-serif;
+       font-feature-settings: 'liga' 1, 'calt' 1; /* fix for Chrome */
+    }
+
+    @supports (font-variation-settings: normal) {
+      :root { font-family: InterVariable, sans-serif; }
+    }
 `;
