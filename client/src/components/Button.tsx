9fc4198123928be8d29f02207ef104e4e4bc3d03
lukaswerner <sherpa.psx@gmail.com>
2024-10-11 09:45:58 +0200

    F9: Button component implemented

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,3 +1,41 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Button = styled.button``;
+export interface ButtonProps {
+    variant?: "primary" | "error" | "success";
+}
+
+export const Button = styled.button<ButtonProps>`
+    padding: 0.5rem 1rem;
+    border-radius: 4px;
+    cursor: pointer;
+    border: none;
+    display: inline-flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    ${(props) =>
+        props.variant === "success" &&
+        css`
+            background-color: blue;
+            color: white;
+        `}
+
+    ${(props) =>
+        props.variant === "primary" &&
+        css`
+            background-color: blue;
+            color: white;
+        `}
+    ${(props) =>
+        props.variant === "error" &&
+        css`
+            background-color: red;
+            color: white;
+        `}
+        ${(props) =>
+        !props.variant &&
+        css`
+            background-color: gray;
+            color: white;
+        `};
+`;
